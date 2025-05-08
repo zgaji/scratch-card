18cc0522d4b8c77fb76c52f7e3f4d7933a4d2602
zgaji <zarinelle.c@gmail.com>
2025-05-09 00:19:30 +0800

    color changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Fill canvas with gray overlay
-ctx.fillStyle = '#FFC1DA';
+ctx.fillStyle = '#bfdda8';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Erasing mode
